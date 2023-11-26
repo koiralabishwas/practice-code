97cb31f8e03dd42075d28106c76b0862239298af
BishwasKoirala <wasubisu69@gmail.com>
2023-11-26 15:41:43 +0900

    commit

---
+++ test.js
@@ -0,0 +1,10 @@
+function tripleLoop() {
+  for (let i = 1;i <= 10 ; i ++) {
+    for (let y = 1; y <= 10 ; y ++) {
+      let result = i * y
+      console.log(result)
+    }
+  }
+}
+
+tripleLoop()
