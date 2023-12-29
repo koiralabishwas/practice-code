8997e6e69c00884651d89c9264d1bd3526482fbd
BishwasKoirala <wasubisu69@gmail.com>
2023-12-30 01:13:45 +0900

    doing shifts onlu problem

---
+++ atcoder/begineersSelection/3shiftonly.js
@@ -0,0 +1,15 @@
+// inputに入力データ全体が入る
+function Main(input) {
+
+}
+
+input = `3 4
+`
+
+Main(input)
+
+
+
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+// Main(require("fs").readFileSync("/dev/stdin", "utf8"));
