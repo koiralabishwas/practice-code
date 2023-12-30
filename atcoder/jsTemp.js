1c07687eec4d8e6c4ae6aed862e4e1f86b3fb8c8
BishwasKoirala <wasubisu69@gmail.com>
2023-12-31 02:07:25 +0900

    commiting missed files

---
+++ atcoder/jsTemp.js
@@ -0,0 +1,17 @@
+// inputに入力データ全体が入る
+function Main(input) {
+	// 1行目がinput[0], 2行目がinput[1], …に入る
+	input = input.split("\n");
+	let tmp = input[1].split(" ");
+	
+}
+
+
+// custom inputs
+let input = `3 4
+`
+
+Main(input)
+
+// to run at atcoder
+// Main(require("fs").readFileSync("/dev/stdin", "utf8"));
