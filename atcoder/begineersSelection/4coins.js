39a3becc102f44a3397054923997063ad0ef1b87
BishwasKoirala <wasubisu69@gmail.com>
2024-01-06 02:03:00 +0900

    do the 4coins problem next

--- atcoder/begineersSelection/4coins.js
+++ atcoder/begineersSelection/4coins.js
@@ -1,37 +1,21 @@
-let input =`30
-40
-50
-6000
-`
+let input =`20 2 5`
 // inputに入力データ全体が入る
 function Main(input) {
 	// 1行目がinput[0], 2行目がinput[1], …に入る
-	input = input.split("\n");
+	input = input.split(" ");
 
-  const aInput = parseInt(input[0],10) 
-  const bInput = parseInt(input[1],10)
-  const cInput = parseInt(input[2],10)
-  const xInput = parseInt(input[3],10)
+  let vN = parseInt(input[0],10)
+  let vA = parseInt(input[1],tmu)
+  let vB = parseInt(input[2],10)
 
-  let count = 0
+  let totlaSum = 0
+  
 
-  // funtion  to  process
-  function countWays(A,B,C,X) {
-    for (let a = 0 ; a <= A ; a ++ ) {
-      for (let b = 0 ; b <= B ; b ++) {
-        for(let c = 0 ; c <= C ; c++) {
+  console.log(input)
+  console.log(vN,vA,vB)
 
 
-          if (500 * a + 100 * b + 50 * c === X){
-            count ++
-          }
-        }
-      }
-    }
-  }
 
-  countWays(aInput,bInput,cInput,xInput)
-  console.log(count)
 }
 
 
