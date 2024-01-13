479e05d54e3be01e208fd9ec4883bba7b600c301
BishwasKoirala <wasubisu69@gmail.com>
2024-01-13 13:48:08 +0900

    do 4 coin

--- atcoder/begineersSelection/4coins.js
+++ atcoder/begineersSelection/4coins.js
@@ -1,21 +1,20 @@
-let input =`20 2 5`
+let input =`2
+2
+2
+100`
 // inputに入力データ全体が入る
 function Main(input) {
 	// 1行目がinput[0], 2行目がinput[1], …に入る
-	input = input.split(" ");
+	input = input.split("\n");
+  
+  let a = parseInt(input[0],10)
+  let b = parseInt(input[1],10)
+  let c = parseInt(input[2],10)
+  let x = parseInt(input[3],10)
 
-  let vN = parseInt(input[0],10)
-  let vA = parseInt(input[1],tmu)
-  let vB = parseInt(input[2],10)
+  console.log(a,b,c,x)
 
   let totlaSum = 0
-  
-
-  console.log(input)
-  console.log(vN,vA,vB)
-
-
-
 }
 
 
