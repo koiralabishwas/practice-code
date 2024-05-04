ee4db5a90bbdd09e0f6911bad8df05904178f1eb
BishwasKoirala <wasubisu69@gmail.com>
2024-05-04 21:14:42 +0900

    asdf

--- mosh-js/index.js
+++ mosh-js/index.js
@@ -1,42 +1,12 @@
-const marks = [0, 0,90] 
-
-function calculateGrade (marks) {
-  let total = 0
-  for (let i = 0; i < marks.length ; i ++) {
-    total += marks[i]
+function showStars (rows){
+  for ( let i = 1 ; i <= rows ; i ++) {
+    let stars = ''
+    for (let j = 0 ; j <i ; j ++) {
+      stars += "*"
+    }
+    console.log(stars)
+    
   }
-  const average = total / marks.length
-  console.log(average)
-
-  let grade = (average <= 59) ? "F" : (average <= 69) ? "D" : (average <= 79) ? 'C' : (average <= 89) ? "B" : "A"
-
-  return grade
 }
 
-
-
-
-
-
-
-
-
-
-function calculateGradev2 (marks) {
-  
-  const average = getAverage(marks)
-
-  let grade = (average <= 59) ? "F" : (average <= 69) ? "D" : (average <= 79) ? 'C' : (average <= 89) ? "B" : "A"
-
-  return grade
-}
-
-function getAverage (marks) {
-  let total = 0
-  for (let mark of marks) {
-    total += mark
-  }
-  return total / marks.length
-}
-
-console.log(calculateGradev2(marks))
+showStars(6)
