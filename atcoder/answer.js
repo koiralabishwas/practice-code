94acc19118821959b1231b61e533395088a97eee
Bishwas Koirala <wasubisu69@gmail.com>
2024-07-11 14:26:40 +0900

    made env for doing javascript and pthd

--- atcoder/answer.js
+++ atcoder/answer.js
@@ -3,6 +3,7 @@ import { readFileSync } from 'fs';
 // Read input file
 const input = readFileSync('input.txt', 'utf8').split('\n');
 
+// code here
 
 // Output results
 console.log(input);
--- atcoder/answer.py
+++ atcoder/answer.py
@@ -3,6 +3,8 @@ import sys
 # Redirect standard input to read from a file
 sys.stdin = open('input.txt', 'r')
 
+#code here 
 a = input()
 b = input()
 print(b)
+
