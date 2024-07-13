f9c75801021e7213f16d49ede17c18a60ce9dbea
Bishwas Koirala <wasubisu69@gmail.com>
2024-07-14 01:07:30 +0900

    solved atcoders

--- atcoder/answer.js
+++ atcoder/answer.js
@@ -1,9 +1,25 @@
-import { readFileSync } from 'fs';
+const fs = require('fs');
 
-// Read input file
-const input = readFileSync('input.txt', 'utf8').split('\n');
+// Read the input from a file
+// FIXME:change to '/dev/stdin' when submitting to atcoder
+const input = fs.readFileSync('input.txt', 'utf8').split('\n');
 
-// code here
+const a500 = parseInt(input[0]);
+const b100 = parseInt(input[1]);
+const c50 = parseInt(input[2]);
+const total = parseInt(input[3]);
 
-// Output results
-console.log(input);
+let counter = 0;
+
+for (let i = 0; i <= a500; i++) {
+    for (let j = 0; j <= b100; j++) {
+        for (let k = 0; k <= c50; k++) {
+            const sumValue = i * 500 + j * 100 + k * 50;
+            if (sumValue === total) {
+                counter++;
+            }
+        }
+    }
+}
+
+console.log(counter);
--- atcoder/answer.py
+++ atcoder/answer.py
@@ -4,18 +4,7 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 #########code from here here --------
-
-c500 = int(input())
-c100 = int(input())
-c50 = int(input())
-total = int(input())
-
-methods = 0
-
-temp = 0
-for i in range(0 , c500) :
-    for j in range(0 , c100) :
-        for k in range( 0 , c50) :
-            print(c50)
-        print(c100)
-    print(c500)
+def main() :
+    N , A , B = map(int, input().split())
+    
+main()
