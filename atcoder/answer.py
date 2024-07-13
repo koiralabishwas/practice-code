618664d70320d805421d691f8d16138872dcd388
Bishwas Koirala <wasubisu69@gmail.com>
2024-07-13 20:50:26 +0900

    solved algo prob

--- atcoder/answer.py
+++ atcoder/answer.py
@@ -3,8 +3,40 @@ import sys
 # Redirect standard input to read from a file
 sys.stdin = open('input.txt', 'r')
 
-#code here 
-a = input()
-b = input()
-print(b)
+#########code from here here --------
+len = int(input())
+datas = input().split(" ")
+
+# num list
+numlist = []
+for d in datas :
+    numlist.append(int(d))
+
+
+# functions-------------
+def allEven(arrray) :
+    for i in arrray :
+        if i % 2 != 0 :
+            return False
+    return True
+
+def divBy2(array) :
+    return [i // 2 for i in array]
+
+# main
+counter = 0
+while allEven(numlist) :
+    numlist = divBy2(numlist)
+    counter += 1 
+
+    if all(v == 0 for v in numlist) :
+        break
+
+
+print(counter)
+
+
+
+
+    
 
