ed3ebc8e1828ce48c3b96519f9348a120cec1ae2
Bishwas Koirala <wasubisu69@gmail.com>
2024-07-15 17:11:23 +0900

    did some ex

--- atcoder/answer.py
+++ atcoder/answer.py
@@ -4,11 +4,23 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 #########code from here here --------
-def main() :
-    n , a , b = map(int, input().split())
-    n = str(n).split()
+def main():
+    n, a, b = map(int, input().split())
 
-    print(n , a ,b)
+
+    sum_total = 0
+
+    for i in range(1, n + 1):
+        digit_sum = sum(int(digit) for digit in str(i))
+        if a <= digit_sum and digit_sum <= b:
+            sum_total += i
+    print(sum_total)
+                
+
+
+
+
+        
         
 
 main()
