618ce1c29450e483d467649fd437ea4ffc14ece2
Bishwas Koirala <wasubisu69@gmail.com>
2024-09-25 23:59:13 +0900

    practice

--- atcoder/answer.py
+++ atcoder/answer.py
@@ -5,27 +5,6 @@ sys.stdin = open('input.txt', 'r')
 
 #########code from here here --------
 def main():
-    N = int(input()) # number of cards 
-    cards = list(map(int , input().split())) # card values
-    cards.sort(reverse=True)
-
-    alice = 0
-    bob = 0
-
-
-    i = 0
-    while i < len(cards) :
-        alice += cards[i]
-        if i + 1 < len(cards) :
-            bob += cards[i + 1]
-        else :
-            break 
-        i += 2
-    
-    # print(cards)
-    # print(alice)
-    # print(bob)
-    print(alice - bob)
-
+    print(input().replace(".",""))
 
 main()
