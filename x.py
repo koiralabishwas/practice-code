b064a22c02a180127fc55b4d8079715851ee25f5
Bishwas Koirala <wasubisu69@gmail.com>
2024-12-06 15:46:41 +0900

    did some code for my school test . too begineer level code

--- x.py
+++ x.py
@@ -1,1 +1,23 @@
-print("hello world")
+# def hantei(t) :
+#     if t >= 70 :
+#         print("合格")
+#     else :
+#         print("不合格")
+
+# ten = int(input("tensu")
+# )
+# hantei(ten)
+
+def keisan(x , y ,z) :
+    ans = (x * 100) + (y * 10) + z
+    return ans
+
+
+
+x = int(input("tensu"))
+y = int(input("tensu"))
+z = int(input("tensu"))
+
+m = keisan(x , y ,z)
+
+print(m)
