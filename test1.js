1154ff6951fa0af3e0fabe1cccc0aaa5ad8f7899
Bishwas Koirala <wasubisu69@gmail.com>
2025-05-08 22:07:17 +0900

    i can add fuctions in arrays in js . wadu hek

--- test1.js
+++ test1.js
@@ -1,8 +1,12 @@
-const numbers = [1, 2, [3, 4, 5,[ 6, 7, 8, 9], 2, 1, 4], 3, 7];
-console.log(numbers)
+// const numbers = [1, 2, [3, 4, 5,[ 6, 7, 8, 9], 2, 1, 4], 3, 7];
+// console.log(numbers)
 
-// let map = numbers.map((i) => );
-let depth = Infinity
-let flat = numbers.flat(depth)
+// // let map = numbers.map((i) => );
+// let depth = Infinity
+// let flat = numbers.flat(depth)
 
-console.log(flat);
+// console.log(flat);
+
+const arrWithfunc = [() => console.log("can i do it ??") , "hello"]
+
+console.log(arrWithfunc[0]())
